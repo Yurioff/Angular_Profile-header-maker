5895c0d3d1542d7fe2683eccf61e6f19ca53f950
Yuri Moreira <yuri.moreira803@gmail.com>
2023-10-11 01:15:32 -0300

    update style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import {MatTabsModule} from '@angular/material/tabs';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 
 
@@ -28,5 +29,22 @@ export class MainComponent {
   borderRadius = "10"
   borderType:string = "hidde"
 
+  //  components menu icon
+  iconBg = ''
+
+  mudarIcon(novaUrl: string) {
+    this.iconBg = novaUrl;
+  }
+
+  @ViewChild('divRef') divRef:any;
+
+  onDownload() {
+    html2canvas(this.divRef.nativeElement).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = 'imagem.png';
+      link.href = canvas.toDataURL();
+      link.click();
+    });
+  }
 
 }
